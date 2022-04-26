9ccf29aee013b5884eda65fdbf091492e40b1106
nukit18 <vip.nukit@mail.ru>
2022-04-27 00:11:43 +0500

    task-basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,21 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    const dict = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+    for (let i = 0; i < str.length - 1; i += 1) {
+        if (dict[str[i]] >= dict[str[i + 1]]) {
+            result += dict[str[i]];
+        } else {
+            result -= dict[str[i]];
+        }
+    }
+    result += dict[str[str.length - 1]];
     return result;
 }
